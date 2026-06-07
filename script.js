14952174f1aced931c466b6fcb9600a3903ee89e
Rajdeep8106 <jujarurajdeep@gmail.com>
2026-06-07 18:08:06 +0530

    Third File with softreset of last commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// This is a single line comment in js //
+/*
+This is a multi-line comment in js
+*/
+
+console.log("Hello, World!"); // This will print "Hello, World!" to the console
+let name = "Alice"; // This is a variable declaration
+console.log("Hello, " + name + "!"); // This will print "Hello, Alice!" to the console
